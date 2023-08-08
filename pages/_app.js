fded6643ed3465621045c178c0fbbffbe397bb3f
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-08-08 17:43:06 +0300

    signup form

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import '@/styles/globals.css'
-import {AuthProvider} from '../context/auth'
+import "@/styles/globals.css";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+import { AuthProvider } from "../context/auth";
 
 export default function App({ Component, pageProps }) {
-  return <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>
-  
-
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
