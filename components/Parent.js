ad21828c48d2cca772cf152cd3a045ed41cc993a
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-08-08 17:46:11 +0000

    signin, our team, about us done

--- components/Parent.js
+++ components/Parent.js
@@ -3,11 +3,11 @@ import Header from './Header'
 import Footer from './Footer'
 
 
-export const Parent = ({children}) => {
+export const Parent = () => {
   return (
     <>
     <Header/>
-     {children}
+     {/* {children} */}
     <Footer/>
     </>
   )
--- pages/signin.js
+++ pages/signin.js
@@ -47,79 +47,89 @@ export default function signIn() {
   return (
 
 
-<div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg light:bg-gray-800 lg:max-w-4xl mt-10" >
-    <div className="hidden bg-cover lg:block lg:w-1/2" style={{
-  backgroundImage: `url('https://img.freepik.com/free-photo/man-with-tablet_1112-648.jpg?w=1380&t=st=1691495687~exp=1691496287~hmac=502d84b9091a2d6fe8c9ee8d7c1920bd0eb1ed9d6be15de3c295222d984aca8a')`,
-  backgroundPosition :'center',
-}}
-></div>
-
-    <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
-        <div className="flex justify-center mx-auto">
-            <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt=""/>
-        </div>
-
-        <p className="mt-3 text-xl text-center text-black light:text-black">
-            Welcome back!
-        </p>
-
-        <a href="#" className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <div className="px-4 py-2">
-                {/* <svg className="w-6 h-6" viewBox="0 0 40 40">
-                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
-                    <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
-                    <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
-                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
-                </svg> */}
-            </div>
-
-            <span className="w-5/6 px-4 py-3 font-bold text-center text-black">Sign in with Google</span>
-        </a>
-
-        <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-
-            <a href="#" className="text-xs text-center text-black uppercase dark:text-gray-400 hover:underline">or login
-                with email</a>
-
-            <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
-        </div>
-
-        <form onSubmit={userHandler}>
-        <div className="mt-4" >
-          
-            <label className="block mb-2 text-sm font-medium text-black " for="LoggingEmailAddress">User Name</label>
-            <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="Name" />
-            {errors.user_name && <p>{errors.user_name}</p>}
-        </div>
-
-        <div className="mt-4">
-            <div className="flex justify-between">
-                <label className="block mb-2 text-sm font-medium text-black " for="loggingPassword">Password</label>
+<div className="bg-gray-200 min-h-screen flex-1 pt-10">
+        <div className="container mx-auto">
+          <div className="flex justify-center px-6 my-12">
+            {/* <!-- Row --> */}
+            <div className="w-full xl:w-3/4 lg:w-11/12 flex">
+              {/* <!-- Col --> */}
+              <div
+                className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+                style={{
+                  backgroundImage: `url(
+                    "https://img.freepik.com/free-photo/man-with-tablet_1112-648.jpg?w=1380&t=st=1691495687~exp=1691496287~hmac=502d84b9091a2d6fe8c9ee8d7c1920bd0eb1ed9d6be15de3c295222d984aca8a"
+                  )`,
+                  backgroundPosition: "center",
+                }}
+              >
+                {/* <img src="https://img.freepik.com/free-psd/newspaper-cover-concept-mock-up_23-2148384363.jpg?w=826&t=st=1691495160~exp=1691495760~hmac=0111bb20b93a1588a5c1c27312eaf41d8f86d22a86e3e286be8fea750599891d" /> */}
+              </div>
+              {/* Col  */}
+              <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+                <h3 className="pt-4 text-2xl text-center">
+                 Signin to your account!
+                </h3>
+                <form
+                  className="px-8 pt-6 pb-8 mb-4 bg-white rounded"
+                  onSubmit={userHandler}
+                >
+               
+                  <div className="mb-4">
+                    <label className="block mb-2 text-sm font-bold text-gray-700">
+                      Username
+                    </label>
+                    <input
+                      className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                      id="username"
+                      type="text"
+                      placeholder="Username"
+                      name="Name"
+                      required
+                    />
+                    {errors.user_name && <p>{errors.user_name}</p>}
+                  </div>
+               
+                  <div className="mb-4 md:flex md:justify-between">
+                    <div className="mb-4 md:mr-2 md:mb-0">
+                      <label className="block mb-2 text-sm font-bold text-gray-700">
+                        Password
+                      </label>
+                      <input
+                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 borderrounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="******************"
+                        name="Pass"
+                        required
+                      />
+                      {errors.password && <p>{errors.password}</p>}
+                    </div>
+                 
+                  </div>
+                  <div className="mb-6 text-center">
+                    <button
+                      className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                      type="submit"
+                    >
+                      Register Account
+                    </button>
               
+                  </div>
+                  <hr className="mb-6 border-t" />
+                  <div className="text-center">
+                    <Link
+                      className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                      href="/signup"
+                    >
+                      Don't have an account? Signup!
+                    </Link>
+                  </div>
+                </form>
+              </div>
             </div>
-
-            <input id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="Pass"/>
-            {errors.password && <p>{errors.password}</p>}
+          </div>
         </div>
-       
-
-        <div className="mt-6">
-            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                Sign In
-            </button>
-        </div>
-        </form>
-
-        <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-
-            <a href="/signup" className="text-xs text-black uppercase dark:text-black hover:underline">or sign up</a>
-
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-        </div>
-    </div>
-</div>
+      </div>
  
   )
 }
