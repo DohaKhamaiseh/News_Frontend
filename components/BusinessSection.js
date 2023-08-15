4131a97e99227fa27f2b4ec4da73912ca5ce3d76
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-08-15 13:35:43 +0300

    lang

--- components/BusinessSection.js
+++ components/BusinessSection.js
@@ -23,11 +23,21 @@ export default function Business() {
       <hr className="mb-6 border-t border-gray-300 dark:border-gray-600" />{" "}
       <h1 className="text-2xl dark:text-white text-black flex  pt-10  ">
         <span className="w-4 bg-black mx-2 title_box"> </span>
-        Business
-        <span className="pt-1 pl-1">
-          {" "}
-          <FaAngleRight />{" "}
-        </span>{" "}
+        {t("home:business_cat") === "Business" ? (
+          <>
+            {t("home:business_cat")}
+            <span className="pt-1 pl-1">
+              <FaAngleRight />{" "}
+            </span>
+          </>
+        ) : (
+          <>
+            {t("home:business_cat")}
+            <span className="pt-1 pl-1">
+              <FaAngleLeft />{" "}
+            </span>
+          </>
+        )}
       </h1>
       {!dataAr ? (
         <Loader />
