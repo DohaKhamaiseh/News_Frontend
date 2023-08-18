e1bb124681aee50a7c6cbd7d04dde853256de5dc
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-08-18 19:51:30 +0300

    names

--- pages/Aboutus.js
+++ pages/Aboutus.js
@@ -1,6 +1,7 @@
 import { Parent } from "@/components/Parent";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Image from "next/image";
 
 export async function getStaticProps({ locale }) {
   return {
